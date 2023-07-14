e78d514203f1a32ea49c18bb055be1e8454f8c99
harrisontaee <harrisontaee@protonmail.com>
2023-07-14 17:12:44 +0100

    tries

---
+++ tries/implement-trie/index.ts
@@ -0,0 +1,48 @@
+class TrieNode {
+    children: Map<string, TrieNode>
+    isEnd: boolean;
+    constructor () {
+        this.children = new Map<string, TrieNode>();
+        this.isEnd = false;
+    };
+};
+
+
+
+
+
+class Trie {
+    root: TrieNode;
+    constructor() {
+        this.root = new TrieNode();
+    };
+
+
+    private set(str: string): void {
+        let cur = this.root;
+
+        for (let char of str) {
+            if (!cur.children.has(char)) cur.children.set(char, new TrieNode());
+            cur = cur.children.get(char);
+        };
+
+        cur.isEnd = true;
+    };
+
+
+    private get(str: string): TrieNode | null {
+        let cur = this.root;
+
+        for (let char of str) {
+            if (!cur.children.has(char)) return null;
+            cur = cur.children.get(char);
+        };
+
+        return cur;
+    };
+
+	 
+    insert = (str: string): void => this.set(str);
+    search = (str: string): boolean => !!this.get(str)?.isEnd;
+    startsWith = (str: string): boolean =>  !!this.get(str);
+};
